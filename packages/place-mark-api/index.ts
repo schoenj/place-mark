510f827020576e0b9f68aa0593c41a6c2f256c53
Johann Schoen <johann@jschoen.de>
2023-05-16 08:55:16 +0200

    Adds Eslint with airbnb-config (and moditifcations for correct ussage with typescript) to the place-mark-api

--- packages/place-mark-api/index.ts
+++ packages/place-mark-api/index.ts
@@ -1,5 +1,5 @@
-import { createServer$ } from "./server";
 import { PrismaClient } from "@prisma/client";
+import { createServer$ } from "./server";
 
 const prisma = new PrismaClient();
 createServer$(prisma).then(({ start$ }) => start$());
