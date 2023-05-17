bf5b6a09ed4bc7b9916cc4a7fcd0ff734d39ea5e
Johann Schoen <johann@jschoen.de>
2023-05-17 13:38:24 +0200

    Adding ISignUpUserRequestDto and joi validation schema

---
+++ packages/place-mark-api/app/core/dtos/user/ISignUpUserRequestDto.ts
@@ -0,0 +1,7 @@
+export interface ISignUpUserRequestDto {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  passwordAgain: string;
+}
--- packages/place-mark-api/app/core/index.ts
+++ packages/place-mark-api/app/core/index.ts
@@ -2,3 +2,5 @@ export * from "./config.js";
 export * from "./dependency-management.js";
 export * from "./rendering.js";
 export * from "./types.js";
+export * from "./dtos/index.js";
+export * from "./validation/index.js";
