ac6f4f22b8cc8b61d702ee17bd60d70117a4cf36
Johann Schoen <johann@jschoen.de>
2023-06-18 18:48:27 +0200

    Adds 404 test for GET /api/user/{id}

--- packages/place-mark-api/tests/api-controllers/user-api-controller.spec.ts
+++ packages/place-mark-api/tests/api-controllers/user-api-controller.spec.ts
@@ -1,6 +1,6 @@
 import { Server, ServerApplicationState } from "@hapi/hapi";
 import { PrismaClient } from "@prisma/client";
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
 import { assert } from "chai";
 import { createServer$ } from "../../app/server.js";
 import { Container, getConfig, IUserReadOnlyDto } from "../../app/core/index.js";
@@ -14,7 +14,7 @@ suite("UserApiController Integration Tests", () => {
     await prismaClient.$connect();
     const result = await createServer$(getConfig(), () => new Container(prismaClient));
     server = result.server;
-    await result.start$();
+    await result.start$(false);
   });
 
   teardown(async () => {
@@ -23,28 +23,41 @@ suite("UserApiController Integration Tests", () => {
     await prismaClient.$disconnect();
   });
 
-  test("GET /api/user/{id} should work", async () => {
-    const user = await prismaClient.user.create({
-      data: {
-        firstName: "Cookie",
-        lastName: "Monster",
-        email: "cookie.monster@sesame-street.de",
-        password: "1234qwer",
-        admin: false,
-      },
+  suite("GET /api/user/{id} Tests", () => {
+    test("should return 404", async () => {
+      try {
+        await axios.get(`${server.info.uri}/api/user/646634e51d85e59154d725c5`);
+      } catch (err) {
+        assert.isTrue(axios.isAxiosError(err));
+        const axiosError = err as AxiosError;
+        assert.isNotNull(axiosError);
+        assert.equal(axiosError.response?.status, 404);
+      }
     });
 
-    const response: AxiosResponse<IUserReadOnlyDto> = await axios.get(`${server.info.uri}/api/user/${user.id}`);
-    assert.equal(response.status, 200);
-    assert.isNotNull(response.data);
-    assert.equal(response.data.id, user.id);
-    assert.equal(response.data.firstName, user.firstName);
-    assert.equal(response.data.lastName, user.lastName);
-    assert.equal(response.data.email, user.email);
-    assert.equal(response.data.admin, user.admin);
-    response.data.createdAt = new Date(response.data.createdAt);
-    response.data.updatedAt = new Date(response.data.updatedAt);
-    assert.equal(response.data.createdAt.toUTCString(), user.createdAt.toUTCString());
-    assert.equal(response.data.updatedAt.toUTCString(), user.updatedAt.toUTCString());
+    test("should work", async () => {
+      const user = await prismaClient.user.create({
+        data: {
+          firstName: "Cookie",
+          lastName: "Monster",
+          email: "cookie.monster@sesame-street.de",
+          password: "1234qwer",
+          admin: false,
+        },
+      });
+
+      const response: AxiosResponse<IUserReadOnlyDto> = await axios.get(`${server.info.uri}/api/user/${user.id}`);
+      assert.equal(response.status, 200);
+      assert.isNotNull(response.data);
+      assert.equal(response.data.id, user.id);
+      assert.equal(response.data.firstName, user.firstName);
+      assert.equal(response.data.lastName, user.lastName);
+      assert.equal(response.data.email, user.email);
+      assert.equal(response.data.admin, user.admin);
+      response.data.createdAt = new Date(response.data.createdAt);
+      response.data.updatedAt = new Date(response.data.updatedAt);
+      assert.equal(response.data.createdAt.toUTCString(), user.createdAt.toUTCString());
+      assert.equal(response.data.updatedAt.toUTCString(), user.updatedAt.toUTCString());
+    });
   });
 });
