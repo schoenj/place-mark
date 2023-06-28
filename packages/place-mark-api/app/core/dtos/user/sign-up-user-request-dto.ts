95fb40c11242b3d25a2502a13c09e1f7c15eee23
Johann Schoen <johann@jschoen.de>
2023-06-28 08:06:11 +0200

    Remove old eslint-disbale-rule

--- packages/place-mark-api/app/core/dtos/user/sign-up-user-request-dto.ts
+++ packages/place-mark-api/app/core/dtos/user/sign-up-user-request-dto.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { ICreateUserReadWriteDto } from "./create-user-read-write-dto.js";
 
 export interface ISignUpUserRequestDto extends ICreateUserReadWriteDto {
